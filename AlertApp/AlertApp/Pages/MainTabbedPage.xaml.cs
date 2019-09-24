3048d8b0d52851f8fcbc25412c1df15e171203ed
Thanos Argyrakis <t.argyrakis@qubiteq.gr>
2019-09-24 12:25:46 +0300

    request permissions on app start up

--- AlertApp/AlertApp/Pages/MainTabbedPage.xaml.cs
+++ AlertApp/AlertApp/Pages/MainTabbedPage.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using AlertApp.Infrastructure;
 using AlertApp.ViewModels;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -39,7 +41,40 @@ namespace AlertApp.Pages
                         break;
                 }
             }
-        } 
+
+            RequestPermissions();
+        }
+
+        private async void RequestPermissions()
+        {
+            try
+            {
+                var locationPermissionStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
+                var contactPermissionStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Contacts);
+                if (locationPermissionStatus != PermissionStatus.Granted || contactPermissionStatus != PermissionStatus.Granted)
+                {
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(new[] { Permission.Location, Permission.Contacts });
+                }
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }                        
+        }
+
         //protected override bool OnBackButtonPressed()
         //{
         //    return true;
