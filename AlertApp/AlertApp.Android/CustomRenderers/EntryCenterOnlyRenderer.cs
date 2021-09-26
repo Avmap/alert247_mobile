5825669170560401a800dbf1b9596515039cc78d
Kostas Magalios <kmagalios@gmail.com>
2021-09-26 08:06:52 +0300

    #40 Fall detection setting fix
    Plus ui changes

--- AlertApp/AlertApp.Android/CustomRenderers/EntryCenterOnlyRenderer.cs
+++ AlertApp/AlertApp.Android/CustomRenderers/EntryCenterOnlyRenderer.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using AlertApp.Droid.CustomRenderers;
+﻿using AlertApp.Droid.CustomRenderers;
 using AlertApp.Infrastructure;
-using Android.App;
 using Android.Content;
-using Android.OS;
-using Android.Runtime;
 using Android.Views;
-using Android.Widget;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
@@ -29,7 +21,6 @@ namespace AlertApp.Droid.CustomRenderers
             {
                 Control.Gravity = GravityFlags.CenterHorizontal;             
             }
-
         }
     }
 }
--- AlertApp/AlertApp.iOS/CustomRenderers/EntryCenteredOnlyRenderer.cs
+++ AlertApp/AlertApp.iOS/CustomRenderers/EntryCenteredOnlyRenderer.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using AlertApp.Infrastructure;
+using AlertApp.iOS.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EntryCenterOnly), typeof(EntryCenteredOnlyRenderer))]
 namespace AlertApp.iOS.CustomRenderers
 {
-    public class EntryCenteredOnlyRenderer
+    public class EntryCenteredOnlyRenderer : EntryRenderer
     {
-        public EntryCenteredOnlyRenderer()
+        //public EntryCenteredOnlyRenderer()
+        //{
+        //}
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
         {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.HorizontalAlignment = UIKit.UIControlContentHorizontalAlignment.Center;
+            }
         }
     }
 }
